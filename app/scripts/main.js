a5059e8c3f8ff038c0439d55711d814dac039c76
Ian McGaunn <ianmcgaunn@icloud.com>
2014-11-28 14:08:38 -0500

    added header comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
+/*
+  File: app/styles/main.js
+  91.461 Assignment 8
+  Ian McGaunn, UMass Lowell Computer Science, imcgaunn@cs.uml.edu
+  Copyright (c) 2014 by Ian McGaunn. All rights reserved. May be freely
+  copied or excerpted for educational purposes with credit to the author.
+*/
+
 'use strict';
 
 $(document).ready(function() {
--- app/scripts/table_validation.js
+++ app/scripts/table_validation.js
@@ -1,4 +1,10 @@
-//TODO: HEADER COMMENT
+/*
+  File: app/styles/table_validation.js
+  91.461 Assignment 8
+  Ian McGaunn, UMass Lowell Computer Science, imcgaunn@cs.uml.edu
+  Copyright (c) 2014 by Ian McGaunn. All rights reserved. May be freely
+  copied or excerpted for educational purposes with credit to the author.
+*/
 
 'use strict';
 
